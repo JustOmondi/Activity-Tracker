68719e405bcacf7b3ca7bf5dc6e3a986486b843e
Omondi Ochieng <oa@omondi.co.za>
2023-03-30 17:11:05 +0200

    Add Department and Member week report views

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.getReports, name='reports'),
-    path('<int:report_id>/', views.getReport, name='report'),
+    path('<int:id>', views.getReportId, name='report'),
+    path('<str:dept_number>/<str:report_name>', views.getDepartmentWeekReport, name='department_week_report'),
+    path('<str:member_name>/<str:report_name>', views.getMemberWeekReport, name='member_week_report'),
 ]
--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,18 +1,69 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.db.models import Count
+from django.db.models.functions import ExtractWeekDay
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from backend.reports.models import Report
-from backend.reports.serializers import ReportSerializer
+from backend.structure.serializers import MemberSerializer
+
+from ..structure.models import Department, Subgroup, Member
+
+from .models import Report
+from .serializers import ReportSerializer
 
 @api_view(['GET'])
-def getReport(request, report_id):
-    report = Report.objects.get(id=report_id)
+def getReportId(request, id):
+    report = Report.objects.get(id=id)
     serializer = ReportSerializer(report, many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def getReports(request):
-    reports = Report.objects.all()
-    serializer = ReportSerializer(reports, many=True)
-    return Response(serializer.data)
+def getDepartmentWeekReport(request, dept_number, report_name):
+    report_counts_by_day = Report.objects.annotate(
+        weekday=ExtractWeekDay('created')
+    ).values('weekday', 'name').annotate(
+        count=Count('id')
+    ).filter(
+        weekday__lte=5,  # Only count weekdays (Monday = 1, Friday = 5)
+        name=report_name,
+        member__subgroup__department__department_number=dept_number
+    )
+
+    reports = {}
+    for report_count in report_counts_by_day:
+        reports[report_count['weekday']] = {'name': report_count['name'], 'count': report_count['count']}
+
+    members = Member.objects.filter(subgroup__department__department_number=dept_number)
+    serializer = MemberSerializer(members, many=True)
+
+    result = {
+        'reports': reports,
+        'members': serializer.data
+    }
+
+    return Response(result)
+
+@api_view(['GET'])
+def getMemberWeekReport(request, member_name, report_name):
+    report_counts_by_day = Report.objects.annotate(
+        weekday=ExtractWeekDay('created')
+    ).values('weekday', 'name').annotate(
+        count=Count('id')
+    ).filter(
+        weekday__lte=5,  # Only count weekdays (Monday = 1, Friday = 5)
+        name=report_name,
+        member__underscore_name=member_name
+    )
+
+    reports = {}
+    for report_count in report_counts_by_day:
+        reports[report_count['weekday']] = {'name': report_count['name'], 'count': report_count['count']}
+
+    result = {
+        'reports': reports,
+    }
+
+    return Response(result)
