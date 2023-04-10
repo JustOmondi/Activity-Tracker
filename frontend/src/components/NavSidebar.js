303e7329c6755a67913f7373c5deab6d13757ad5
Omondi Ochieng <oa@omondi.co.za>
2023-04-10 15:27:26 +0200

    Basic sidebar styling done

--- frontend/src/components/NavSidebar.js
+++ frontend/src/components/NavSidebar.js
@@ -5,16 +5,28 @@ import {Link} from "react-router-dom";
 export default function NavSidebar() {
     const { collapseSidebar } = useProSidebar();
 
+    const sideBarBGColour = '#111827';
+
     return (
-        <div className='flex h-full'>
-            <Sidebar>
-                <Menu>
-                    <MenuItem component={<Link to="/" />}> Home </MenuItem>
-                    <MenuItem component={<Link to="/subgroups" />}> Subgroups </MenuItem>
-                    <MenuItem component={<Link to="/members" />}> Members </MenuItem>
-                    <MenuItem component={<Link to="/logout" />}> Logout </MenuItem>
-                </Menu>
-            </Sidebar>
+        <div className='flex h-full pl-7 pb-7 pt-7'> 
+            <div className='sidebar-container p-4 pt-14 flex h-full rounded-3xl bg-gray-900'>
+                <Sidebar backgroundColor={sideBarBGColour} rootStyles={{
+                    backgroundColor: '#111827',
+                    borderRight: `0px solid ${sideBarBGColour}`,
+                }}>
+                    <Menu>
+                        <MenuItem component={<Link to="/" />}> Home </MenuItem>
+                        <MenuItem component={<Link to="/subgroups" />}> Subgroups </MenuItem>
+                        <MenuItem component={<Link to="/members" />}> Members </MenuItem>
+                    </Menu>
+                    <div className='sidebar-footer'>
+                        <Menu>
+                            <MenuItem component={<Link to="/" />}> Logout </MenuItem>
+                        </Menu>
+                    </div>
+                    
+                </Sidebar>
+            </div>
         </div>
     )
 }
