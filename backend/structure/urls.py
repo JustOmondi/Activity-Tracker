75cadd85add54e84cb74669dfa16171aefbae15e
Omondi Ochieng <oa@omondi.co.za>
2023-04-17 02:16:37 +0200

    Update URLs to use query params

--- backend/structure/urls.py
+++ backend/structure/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('departments/', views.getDepartments, name='departments'),
-    path('department/<int:department_number>/', views.getDepartment, name='department'),
+    path('department/details/', views.getDepartment, name='department'),
     
     path('subgroups/', views.getSubgroups, name='subgroups'),
-    path('subgroup/<int:subgroup_number>/', views.getSubgroup, name='subgroup'),
+    path('subgroup/details/', views.getSubgroup, name='subgroup'),
 
     path('members/', views.getMembers, name='members'),
-    path('member/<str:name>/', views.getMember, name='member'),
+    path('member/details/', views.getMember, name='member'),
+    path('member/weekreports/', views.getMemberWeekReports, name='member_week_reports'),
 ]
