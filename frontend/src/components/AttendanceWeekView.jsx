27c180e55859d8d2914110f602e7da943152edde
Omondi Ochieng <oa@omondi.co.za>
2023-04-19 02:04:15 +0200

    Change checkbox label positioning

--- frontend/src/components/AttendanceWeekView.jsx
+++ frontend/src/components/AttendanceWeekView.jsx
@@ -3,33 +3,36 @@ import { Checkbox, Card } from 'antd';
 
 export default function AttendanceWeekView({attendance, reportName, member, color}) {
     const daysOfWeek = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+    const classes = 'mr-1 font-bold scale-[1.1]'
 
     return (
         <div>
             <h3>This week:</h3>
-            <Card style={{display: 'flex'}}>
+            <Card style={{width: '100%', justifyContent: 'space-evenly'}}>
                 {daysOfWeek.map((item, index) => {
                     const dayOfWeek = index + 1
                     const checked = attendance.thisWeek[dayOfWeek]
                     const color = attendance.color
-                    const classes = `ml-2 mb-2 font-bold scale-[1.2] checkbox-${color}`
 
                     return(
-                        <Checkbox key={index} className={classes} defaultChecked={checked}>{item}</Checkbox>
+                        <Checkbox key={index} className={`${classes} checkbox-${color}`} defaultChecked={checked}>
+                            <p className='mt-2'>{item}</p>
+                        </Checkbox>
                     )
                 })}
             </Card>
 
             <h3>Last week:</h3>
-            <Card style={{display: 'flex'}}>
+            <Card style={{width: '100%', justifyContent: 'space-evenly'}}>
                 {daysOfWeek.map((item, index) => {
                     const dayOfWeek = index + 1
                     const checked = attendance.lastWeek[dayOfWeek]
                     const color = attendance.color
-                    const classes = `ml-2 mb-2 font-bold scale-[1.2] checkbox-${color}`
 
                     return(
-                        <Checkbox key={index} className={classes} disabled defaultChecked={checked}>{item}</Checkbox>
+                        <Checkbox key={index} className={`${classes} checkbox-${color}`} disabled defaultChecked={checked}>
+                            <p className='mt-2'>{item}</p>
+                        </Checkbox>
                     )
                 })}
             </Card>
