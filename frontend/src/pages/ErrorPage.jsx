bf924504d0ac9f116c1be66bba468e026b4877cf
Omondi Ochieng <oa@omondi.co.za>
2023-05-05 02:03:32 +0200

    Change text sizing on Error Page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -6,8 +6,8 @@ export default function ErrorPage() {
   return (
     <div className='page h-full pt-8 px-6'>
        <div className='page-inner overflow-auto w-full flex flex-col items-center justify-center'>
-          <h1 style={{fontSize: '15rem'}} className='text-gray-400 -mb-8'>404</h1>
-          <h3 style={{fontSize: '3rem'}}>Page not found</h3>
+          <h1 className='text-gray-400 -mb-8 text-[8em] md:text-[10em] xl:text-[15em]'>404</h1>
+          <h3 className='text-[2em] md:text-[4em] xl:text-[5em]'>Page not found</h3>
           <Link to='/dashboard'>
             <Button 
                 className='bg-black text-white shadow-md flex items-center mt-6' 
