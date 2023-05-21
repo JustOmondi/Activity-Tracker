82e89f5b56ea69d4bb3864c1134b668ef4c0cd0c
Omondi Ochieng <oa@omondi.co.za>
2023-05-21 18:40:40 +0200

    Refactor backend JWT auth functionality

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
 urlpatterns = [
     path('', views.getIndex, name='index'),
-    path('admin/', admin.site.urls),
-    path('api/structure/', include('structure.urls')),
-    path('api/reports/', include('reports.urls')),
+    path('admin', admin.site.urls),
+    path('api/structure', include('structure.urls')),
+    path('api/reports', include('reports.urls')),
     path('api/login', views.login_request, name='login'),
     path('api/logout', views.logout_request, name='logout'),
+    path(
+        'api/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'
+    ),
+    path(
+        'api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'
+    ),
 ]
